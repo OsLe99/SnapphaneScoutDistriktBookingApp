1b3dc2757825e67fc88e9dc36f9cd89ec4a77328
Oscar Lejon <oscar.lejon@campusnykoping.se>
2025-02-26 14:38:18 +0100

    Startsida skapad

--- SnapphaneScoutDistriktBookingApp/MainPage.xaml.cs
+++ SnapphaneScoutDistriktBookingApp/MainPage.xaml.cs
@@ -9,17 +9,37 @@
             InitializeComponent();
         }
 
-        private void OnCounterClicked(object sender, EventArgs e)
+        private void OnBytTillKanot(object sender, EventArgs e)
         {
-            count++;
 
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
-
-            SemanticScreenReader.Announce(CounterBtn.Text);
         }
+
+        private void OnBytTillStuga(object sender, EventArgs e)
+        {
+
+        }
+
+        private void OnBytTillLägerOmråde(object sender, EventArgs e)
+        {
+
+        }
+
+        private void OnBytTillVindskydd(object sender, EventArgs e)
+        {
+
+        }
+
+        //private void OnCounterClicked(object sender, EventArgs e)
+        //{
+        //    count++;
+
+        //    if (count == 1)
+        //        CounterBtn.Text = $"Clicked {count} time";
+        //    else
+        //        CounterBtn.Text = $"Clicked {count} times";
+
+        //    SemanticScreenReader.Announce(CounterBtn.Text);
+        //}
     }
 
 }
